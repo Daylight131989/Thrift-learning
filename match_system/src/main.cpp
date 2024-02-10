ff41c92701e9ef8856c14f05b96f400e68ee66c9
daylight <1661434401@qq.com>
2024-02-10 12:08:40 +0800

    match server:3.0

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -54,7 +54,10 @@ class Pool
             try {
                 transport->open();
 
-                client.save_data("acs_8794", "4b693cc3", a, b);
+                int res = client.save_data("acs_8794", "4b693cc3", a, b);
+
+                if (!res) puts("success");
+                else puts("failed");
 
                 transport->close();
             } catch (TException& tx) {
@@ -66,11 +69,24 @@ class Pool
         {
             while (users.size() > 1)
             {
-                auto a = users[0], b = users[1];
-                users.erase(users.begin());
-                users.erase(users.begin());
+                sort(users.begin(), users.end(), [&](User& a, User& b){
+                        return a.score < b.score;
+                        });
 
-                save_result(a.id, b.id);
+                bool flag = true;
+                for (uint32_t i = 1; i < users.size(); i++)
+                {
+                    auto a = users[i - 1], b = users[i];
+                    if (b.score - a.score <= 50)
+                    {
+                        users.erase(users.begin() + i - 1, users.begin() + i + 1);
+                        save_result(a.id, b.id);
+
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) break;
             }
         }
 
@@ -129,9 +145,11 @@ void consume_task()
         unique_lock<mutex> lck(message_queue.m);
         if (message_queue.q.empty())
         {
-            message_queue.cv.wait(lck);
+            lck.unlock();
+            pool.match();
+            sleep(1);
         }
-        else 
+        else
         {
             auto task = message_queue.q.front();
             message_queue.q.pop();
